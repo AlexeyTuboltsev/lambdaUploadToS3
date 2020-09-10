ab5abdda661caa1de848538389d8e08fd21ead27
AlexeyTuboltsev <tblz@palavara.com>
2020-09-10 13:44:12 +0200

    fixed metadata added fileName generation added deployment script

--- deployment.sh
+++ deployment.sh
@@ -1,3 +1,4 @@
 zip -r ./lambdaUploadToS3.zip ./node_modules ./index.js
 echo "...starting upload"
 aws lambda update-function-code --function-name save-image-to-s3 --zip-file fileb://lambdaUploadToS3.zip  --region us-east-1
+echo "--DONE--"
--- index.js
+++ index.js
@@ -1,63 +1,39 @@
-//stolen from https://medium.com/@olotintemitope/how-to-upload-files-to-amazon-s3-using-nodejs-lambda-and-api-gateway-bae665127907
-
 const AWS = require('aws-sdk');
-const s3 = new AWS.S3();
-const moment = require("moment")
+const s3 = new AWS.S3({region: 'eu-central-1'});
+const crypto = require('crypto');
 const fileType = require("file-type");
-AWS.config.update({region: 'us-east-1'});
 
 const BUCKET_NAME = process.env.BUCKET_NAME
+const BUCKET_IMG_PATH = process.env.BUCKET_IMG_PATH
 
-function getFile({ext, mime}, buffer){
-  const timeStamp = moment().format("YYYY-MM-DD-HH-mm-ss");
-
-  const fileName = `testFile_${timeStamp}.${ext}`;
-
-  const params = {
-    Bucket: BUCKET_NAME,
-    Key: fileName,
-    Body: buffer
-  };
-
-  const meta = {
-    size: buffer.toString('ascii').length,
-    type: mime,
-    name:fileName,
-  }
-
-  return {
-    params,
-    meta
-  }
+function generateRandomString(strLength){
+  return crypto.randomBytes(strLength).toString('hex')
 }
 
-
 exports.handler = async (event, context) => {
-  const request = event.body;
-  const base64String = request.split(';base64,').pop()
-  const buffer = new Buffer(base64String, 'base64');
+  const { file, itemId } = event;
+  const base64String = file.split(';base64,').pop()
+  const buffer = new Buffer.from(base64String, 'base64');
   const fileMime = await fileType.fromBuffer(buffer)
 
   if (fileMime === null || fileMime === undefined){
     return context.fail("invalid file type")
   }
+  const fileName = `${generateRandomString(8)}.${fileMime.ext}`;
 
-  const {params,meta} = getFile(fileMime,buffer)
-  console.log("params,meta",params,meta)
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: `${BUCKET_IMG_PATH}/${fileName}`,
+    ContentType: fileMime.mime,
+    Body: buffer
+  };
+
   return s3.putObject(params,(err,data)=>{
     if(err){
       console.log(err);
       return context.fail(err)
     }
-    return console.log("file_url", meta.full_path)
+    return
   }).promise()
+    .then(()=> ({fileName, itemId}))
 };
-
-// {
-//     "Sid": "Stmt1468366974000",
-//     "Effect": "Allow",
-//     "Action": "s3:*",
-//     "Resource": [
-//         "arn:aws:s3:::my-bucket-name-goes-here/optional-path-before-allow/*"
-//     ]
-// }
